4821e156508c45b859e5c59f2709b1604655baa5
tcollins520 <tcollins520@gmail.com>
2026-07-14 01:25:24 -0400

    fix(adot): associate collector service account with observability namespace

--- terraform/platform/observability/c7_adot_pod_identity_association.tf
+++ terraform/platform/observability/c7_adot_pod_identity_association.tf
@@ -1,7 +1,7 @@
 # ADOT Collector Pod Identity Association
 resource "aws_eks_pod_identity_association" "adot_collector" {
   cluster_name    = data.terraform_remote_state.eks.outputs.eks_cluster_name
-  namespace       = "default"
+  namespace       = "observability"
   service_account = "adot-collector"
   role_arn        = aws_iam_role.adot_collector.arn
   tags            = var.tags
